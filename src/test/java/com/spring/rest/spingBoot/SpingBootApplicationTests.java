38743f0055bde0cd8016a0a0aaa18e667aa359b5
posusaevkostay@mail.ru <posusaevkostay@mail.ru>
2020-01-25 17:36:49 +0300

    Hello World spring boot.

---
+++ src/test/java/com/spring/rest/spingBoot/SpingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.rest.spingBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
